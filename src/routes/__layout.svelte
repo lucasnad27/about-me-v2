7d82dc38552ff5673bd2ac894f719ed53e7d0d77
Lucas Culbertson <lucasnad27@gmail.com>
2022-05-09 15:04:14 -0600

    Main page layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,19 @@
 <script>
-  import "../app.css";
+	import '../app.css';
 </script>
 
 <div class="relative flex h-full min-h-screen w-full">
-  <nav class="absolute flex h-full max-h-screen min-h-screen w-3/4 flex-none transform flex-col overflow-y-auto border-r border-gray-150 bg-white pb-10 transition duration-200 ease-in-out dark:boarder-gray-800 dark:bg-gray-900 sm:w-1/2 sm:pb-0 md:w-1/3 lg:relative lg:z-auto lg:w-56 lg:translate:-x-0 lg:bg-gray-50 lg:dark:bg-gray-900 2xl:w-72"></nav>
+	<nav
+		class="border-gray-150 dark:boarder-gray-800  lg:translate:-x-0 absolute flex h-full max-h-screen min-h-screen w-3/4 flex-none transform flex-col overflow-y-auto border-r bg-white pb-10 transition duration-200 ease-in-out dark:bg-gray-900 sm:w-1/2 sm:pb-0 md:w-1/3 lg:relative lg:z-auto lg:w-56 lg:bg-gray-50 lg:dark:bg-gray-900 2xl:w-72"
+	/>
+	<!-- Start of Unclear what this div does?? -->
+	<div
+		class="opacity-00 pointer-events-none fixed inset-0 z-20 bg-black bg-opacity-0 transition duration-200 ease-in-out dark:bg-opacity-50"
+	/>
+	<!-- End of Unclear what this div does?? -->
+	<div class="flex flex-1">
+		<main class="flex w-full">
+			<slot />
+		</main>
+	</div>
 </div>
-<slot />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,72 @@
-<h1 class="text-purple-500 text-3xl">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="relative flex max-h-screen w-full flex-col overflow-y-auto bg-white dark:bg-black">
+	<!-- Sticky top border goes here -->
+	<!-- End of Sticky top border goes here -->
+	<!-- I'd like to find a better way of applying some space here -->
+	<div class="p-4" />
+	<div class="mx-auto w-full max-w-3xl px-4 pt-12 pb-10 md:px-8">
+		<div class="space-y-8 pb-24 md:space-y-16">
+			<div class="grid grid-cols-1 items-start gap-6 md:grid-cols-12">
+				<div
+					class="col-span-2 pt-8 text-lg font-extrabold text-black dark:text-white md:pt-0 md:text-right md:text-base md:font-normal md:text-opacity-40"
+				/>
+				<div class="col-span-10">
+					<!-- TODO: Add "prose" class within tailwind (using @apply??) that makes for beautiful,
+					easy-to-read text -->
+					<div class="prose prose-cyan">
+						<p>
+							Intro to who you are and what you do. Maybe a <a
+								target="_blank"
+								rel="noopener noreferrer"
+								href="https://github.com/lucasnad27">link</a
+							> here and there.
+						</p>
+						<p>Add some info about what you've done in the past.</p>
+						<p>Add some info about what you did even further in the past</p>
+						<p>Add something you are working on outside of your core work hours</p>
+					</div>
+					<div class="flex pt-6">
+						<a
+							class="hover:text-gray-1000 shadow-xs flex flex-none cursor-pointer items-center justify-center space-x-2 rounded-md border border-gray-400 border-opacity-30 bg-white px-4 py-2 text-sm font-semibold leading-none text-gray-700 opacity-100 transition-all hover:border-opacity-50 hover:shadow-sm dark:border-gray-700 dark:bg-white dark:bg-opacity-10 dark:text-gray-200 dark:hover:border-gray-600 dark:hover:text-white"
+							href="https://www.polywork.com/lucasnad27">View Changelog</a
+						>
+					</div>
+				</div>
+			</div>
+			<div class="grid grid-cols-1 items-start gap-6 md:grid-cols-12">
+				<div
+					class="col-span-2 pt-8 text-lg font-extrabold text-black dark:text-white md:pt-0 md:text-right md:text-base md:font-normal md:text-opacity-40"
+				>
+					Online
+				</div>
+				<div class="col-span-10">
+					<div class="flex-col space-y-3">
+						<a
+							target="_blank"
+							rel="noopener noreferrer"
+							href="https://github.com/lucasnad27"
+							class="group flex items-center space-x-4"
+							><strong
+								class="text-gray-1000 flex-none font-medium group-hover:text-blue-600 group-hover:underline dark:text-gray-100 dark:group-hover:text-blue-500"
+								>GitHub</strong
+							><span
+								class="w-full shrink border-t border-dashed border-gray-300 dark:border-gray-800"
+							/><span class="flex-none text-slate-400">Follow</span></a
+						>
+						<a
+							target="_blank"
+							rel="noopener noreferrer"
+							href="https://github.com/lucasnad27"
+							class="group flex items-center space-x-4"
+							><strong
+								class="text-gray-1000 flex-none font-medium group-hover:text-blue-600 group-hover:underline dark:text-gray-100 dark:group-hover:text-blue-500"
+								>GitHub</strong
+							><span
+								class="w-full shrink border-t border-dashed border-gray-300 dark:border-gray-800"
+							/><span class="flex-none text-slate-400">Follow</span></a
+						>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
