2812c150d97450b703f4a52f9f6f15b6b2a8b77c
Lucas Culbertson <lucasnad27@gmail.com>
2022-05-09 12:22:38 -0600

    postcss + tailwindcss setup

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="text-purple-500 text-3xl">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
