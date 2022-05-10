7b887deb1659841d909d60ad0a37e99c86b5c29f
Lucas Culbertson <lucasnad27@gmail.com>
2022-05-10 16:19:00 -0600

    Implement sidebar

---
+++ src/lib/navBar/SideNavBar.svelte
@@ -0,0 +1,35 @@
+<nav
+	class="3xl:w-80 border-gray-150 absolute z-30 flex h-full max-h-screen min-h-screen w-3/4 flex-none -translate-x-full transform flex-col overflow-y-auto border-r bg-white pb-10 transition duration-200 ease-in-out dark:border-gray-800 dark:bg-gray-900 sm:w-1/2 sm:pb-0 md:w-1/3 lg:relative lg:z-auto lg:w-56 lg:translate-x-0 lg:bg-gray-50 lg:dark:bg-gray-900 2xl:w-72"
+>
+	<div class="flex flex-none items-center justify-between px-6 py-2">
+		<h2 class="transform self-center py-3 text-sm font-bold text-gray-900 line-clamp-1">
+			Lucas Culbertson
+		</h2>
+	</div>
+	<div class="flex-1 space-y-1 px-3 py-3">
+		<ul class="space-y-1">
+			<li class="flex items-stretch space-x-1">
+				<a
+					class="flex flex-1 items-center space-x-3 rounded-md bg-black px-2 py-1.5 text-sm  font-medium text-white hover:bg-black hover:text-white dark:bg-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white"
+					href="/"
+				>
+					<span class="flex w-4 items-center justify-center">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+							width="16"
+							height="16"
+							fill="currentColor"
+							><path
+								d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"
+							/></svg
+						>
+					</span>
+					<span class="flex-1">Home</span>
+				</a>
+			</li>
+			<li class="flex items-stretch space-x-1" />
+			<li class="flex items-stretch space-x-1" />
+		</ul>
+	</div>
+</nav>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,11 +1,10 @@
 <script>
 	import '../app.css';
+	import SideNavBar from '$lib/navBar/SideNavBar.svelte';
 </script>
 
 <div class="relative flex h-full min-h-screen w-full">
-	<nav
-		class="border-gray-150 dark:boarder-gray-800  lg:translate:-x-0 absolute flex h-full max-h-screen min-h-screen w-3/4 flex-none transform flex-col overflow-y-auto border-r bg-white pb-10 transition duration-200 ease-in-out dark:bg-gray-900 sm:w-1/2 sm:pb-0 md:w-1/3 lg:relative lg:z-auto lg:w-56 lg:bg-gray-50 lg:dark:bg-gray-900 2xl:w-72"
-	/>
+	<SideNavBar />
 	<div class="flex flex-1">
 		<main class="flex w-full">
 			<slot />
